40035eb8453a455968403b2c4d140fe89335f79c
David Barroso <dbarrosoc2@gmail.com>
2024-05-26 12:05:13 +0200

    url base

--- common/url-base.php
+++ common/url-base.php
@@ -3,6 +3,6 @@ if ($_SERVER["HTTP_HOST"] === "www.upric.es") {
     $url_base = "/dashboard/";
     $url_base_http = "https://www.upric.es/dashboard/";
 } else if ($_SERVER["HTTP_HOST"] === "localhost" || $_SERVER["HTTP_HOST"] === "127.0.0.1:8000") {
-    $url_base = "/UPRIC/";
-    $url_base_http = "/UPRIC/";
+    $url_base = "/upric-proyecto/";
+    $url_base_http = "/upric-proyecto/";
 }
