8e3b162d05129522618ddd3f8301fbede3bff071
David <dbarrosoc2@gmail.com>
2024-06-15 21:57:48 +0200

    test

--- controllers/database-connection.php
+++ controllers/database-connection.php
@@ -4,19 +4,15 @@ include 'config.php';
 $conn = "";
 try {
     $dsn = "mysql:host=" . $BBDD_HOST . ";dbname=" . $BBDD_NAME . ";charset=utf8";
-
-    echo $dsn;
-    echo $BBDD_PASSWORD;
-    echo $BBDD_USER;
     $conn = new PDO($dsn, $BBDD_USER, $BBDD_PASSWORD);
 
     echo $conn;
+    echo "<br>";
+    echo $dsn;
 } catch (PDOException $e) {
 
     echo "TEST";
     die("Error: " . $e->getMessage() . "<br>");
 }
-
-
 
 return  $conn;
