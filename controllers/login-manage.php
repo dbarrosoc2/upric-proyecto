ecff525d929f3505b22527da7680ff1688f1ba9a
David <dbarrosoc2@gmail.com>
2024-06-15 21:37:42 +0200

    test

--- controllers/login-manage.php
+++ controllers/login-manage.php
@@ -25,7 +25,7 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $stmt = $conn->prepare($query);
         $stmt->execute(['user' => $user, 'pass' => $pass]);
 
-        require "./POO/CLASS/Logs.php";
+        require "POO/CLASS/Logs.php";
         $logs = new RegistroLogger("registroCSVLogin.csv");
 
         if ($stmt->rowCount() > 0) {
