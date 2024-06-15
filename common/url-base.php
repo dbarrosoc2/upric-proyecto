24064d7621787232032005ae2820dc474c2cac2f
David <dbarrosoc2@gmail.com>
2024-06-15 21:04:37 +0200

    fix url base

--- common/url-base.php
+++ common/url-base.php
@@ -1,12 +1,7 @@
 <?php
-echo $_SERVER["HTTP_HOST"];
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-echo "<br>";
-if ($_SERVER["HTTP_HOST"] === "upric.barrosocda.com") {
-    $url_base = "/";
+if ($_SERVER["HTTP_HOST"] === "upric.barrosocda.com" || $_SERVER["HTTP_HOST"] === "www.upric.barrosocda.com") {
+    // $url_base = "/";
+    $url_base = "/dashboard/";
     $url_base_http = "https://www.upric.barrosocda.com/";
 } else if ($_SERVER["HTTP_HOST"] === "localhost" || $_SERVER["HTTP_HOST"] === "127.0.0.1:8000") {
     $url_base = "/upric-proyecto/";
