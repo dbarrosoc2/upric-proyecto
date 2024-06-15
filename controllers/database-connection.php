0b2021cd3c97611ecc8f9b534292b2d40a0b218d
David <dbarrosoc2@gmail.com>
2024-06-15 21:49:20 +0200

    test

--- controllers/database-connection.php
+++ controllers/database-connection.php
@@ -1,6 +1,7 @@
 <?php
 require __DIR__ .'/config.php';
 
+$conn = "test";
 try {
     $dsn = "mysql:host=" . $BBDD_HOST . ";dbname=" . $BBDD_NAME . ";charset=utf8";
     $conn = new PDO($dsn, $BBDD_USER, $BBDD_PASSWORD);
